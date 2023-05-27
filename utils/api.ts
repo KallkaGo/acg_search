c018b5b56f7092b7cb6cd5fe35ab8aad306f4838
KallkaGo <765830637@qq.com>
2023-05-28 00:30:30 +0800

    change:delete console.log

--- utils/api.ts
+++ utils/api.ts
@@ -16,7 +16,6 @@ export function traceMoeSerach( params: IParams | undefined ): Promise<ITracemoe
 			
 			if ( result.ok ) {
 				const res = await result.json();
-				console.log(res);
 				resolve( res );
 			}
 			reject( new Error( "ERROR" ) );
